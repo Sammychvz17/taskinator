5ee7ba7bce45f6c040c230e0ebcc1befffc896c6
sammychvz <sammy_chvz17@outlook.com>
2021-12-02 21:18:10 -0600

    added event function for click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-var taskToDoEl = document.querySelector("#tasks-to-do")
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function () {
-    var listItemEl = document.createElement('li');
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    taskToDoEl.appendChild(listItemEl);
-}
-buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+  console.log(event);
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);

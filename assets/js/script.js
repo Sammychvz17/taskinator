7e181eeb6207a9682be39c795b98deb728de236a
sammychvz <sammy_chvz17@outlook.com>
2021-12-05 16:34:39 -0600

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,6 +63,9 @@ var createTaskEl = function(taskDataObj) {
 
   tasks.push(taskDataObj);
 
+  //local storage
+  saveTasks(); 
+
 
   // add list item to list
   tasksToDoEl.appendChild(listItemEl);
@@ -129,6 +132,9 @@ var completeEditTask = function(taskName, taskType, taskId) {
   }
   }; 
 
+  //local storage
+  saveTasks();
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -179,6 +185,8 @@ for (var i = 0; i < tasks.length; i++) {
 }
 console.log(tasks);
 
+saveTasks();
+
 };
 
 var editTask = function(taskId) {
@@ -209,6 +217,28 @@ var deleteTask = function(taskId) {
   // find task list element with taskId value and remove it
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
+
+  // create new array to hold updated list of tasks
+var updatedTaskArr = [];
+
+// loop through current tasks
+for (var i = 0; i < tasks.length; i++) {
+  // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+  if (tasks[i].id !== parseInt(taskId)) {
+    updatedTaskArr.push(tasks[i]);
+  }
+}
+
+// reassign tasks array to be the same as updatedTaskArr
+tasks = updatedTaskArr;
+
+saveTasks();
+
+};
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
 };
 
 // Create a new task
